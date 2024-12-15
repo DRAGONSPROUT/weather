915f3f62aee9423254069e7cd344c5f80301c29f
Ahijeet <pathakabhijeet786@gmail.com>
2024-12-15 14:59:48 +0530

    Last commit

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: '/weather/',
   entryPoints: {
     '': () => import('./main.server.mjs')
   },
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,18 +2,18 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: '/weather/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/"
+    "route": "/weather"
   }
 ],
   assets: {
-    'index.csr.html': {size: 14390, hash: '2b2b91baec1902e9e98c4cf14dfdcf5f7f0d5719c1300709c958e44d759003cf', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 7964, hash: '73db7919bd6f9c740d8614b8474cc7a3b51b209e8e6836fbc578ef9849d79d05', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 24727, hash: 'de6a038cef581a465e7103a6f5a6e03d35596769e40788a75e8b95a7e6efe4be', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 14398, hash: '5083299018002eb719d02998b3937dd0be5bba02c91b2ce01944ea9c09a9a91f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 7972, hash: 'b749da5c8c797e04d7d4384b8570673c245e892a9bbeff594ee00acfaec56667', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 24735, hash: '1a5ebc9c19c5bfc89ef76237428b7241fa8caa711b98a663b187bdd74821f05d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-CXQUZ3PB.css': {size: 6979, hash: 'mYIPdabeAag', text: () => import('./assets-chunks/styles-CXQUZ3PB_css.mjs').then(m => m.default)}
   },
 };
